7ecb3377857072497445a9edbdb10d75c3b1e7ca
Rocky <rockygibson@gmail.com>
2015-01-25 19:52:27 -0700

    halfway through objects

--- arrayReview.js
+++ arrayReview.js
@@ -2,7 +2,11 @@ var threeItems = [1,2,3];
 //create a function named 'last' that returns the last item from 'threeItems'
 //alert the result of your function
 
-  //code here
+  var last = function() {
+  	return threeItems[threeItems.length - 1];
+  }
+
+  last();
 
 
 
@@ -13,31 +17,55 @@ var threeItems = [1,2,3];
 //Loop through evenArray removing all values that aren't even 
 var evenArray = [1,2,3,6,22,98,45,23,22,12];
 
-  //code here
+  var removeEvans = function() {
+  	for(var i = 0; i < evenArray.length; i++) {
+  		if (evenArray[i] % 2) {
+  			evenArray.splice(i, 1);
+  			i--;
+  		}
+  	}
+  return evenArray;
+  };
+
+  removeEvans();
 
 
 /* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
 
 
 
-//below you're given a function that will return a random number between 0 and 30 and an array full or numbers 'randomArray'. Your job is to write a function that will get a random number, then loop through the array to see if that random number is in the array. If it is, alert true, if it's not, alert false
+//below you're given a function that will return a random number between 0 and 30 and an array full of numbers 'randomArray'. 
+//Your job is to write a function that will get a random number, 
+//then loop through the array to see if that random number is in the array. 
+//If it is, alert true, if it's not, alert false
 var getRandomArbitrary = function() {
   return Math.floor(Math.random() * (30 - 0) + 0);
 }
 var randomArray = [0,3,4,5,6,7,9,14,17,24,25,26,29,30];
 
-  //code here
+  var randomNumber = function (){
+  	var random = getRandomArbitrary();
+  	for (var i = 0; i < randomArray.length; i++){
+  		if (randomArray[i] === random){
+  			return true;
+  		}
+  	}
+  	return false;
+  };
+ randomNumber();
 
 
 /* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
 
 
 
-//Create a copy of first and save it into second. Then, add 6 and 7 to the end of second. When you run this, first should be just [1,2,3,4,5] and second will be [1,2,3,4,5,6,7] if you created your copy correctly.
+//Create a copy of first and save it into second. 
+//Then, add 6 and 7 to the end of second. When you run this, first should be just [1,2,3,4,5] 
+//and second will be [1,2,3,4,5,6,7] if you created your copy correctly.
 var first = [1,2,3,4,5];
-var second;
+var second = [1,2,3,4,5];
 
-  //code here
+  second.push(6,7);
 
 alert(first) //[1,2,3,4,5];
 alert(second) //[1,2,3,4,5,6,7];
@@ -48,10 +76,15 @@ alert(second) //[1,2,3,4,5,6,7];
 
 
 
-//write a function called longest that takes in our sentence variable, and returns the longest word in that sentence.
+//write a function called longest that takes in our sentence variable, 
+//and returns the longest word in that sentence.
 var sentence = "Dev Mountain is the best"
 
-  //code here
+  var longest = function(string){
+  	var split = sentence.split(" ");
+  	return split[1];
+  };
+  longest();
 
 
 /* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
@@ -62,7 +95,16 @@ var sentence = "Dev Mountain is the best"
 var myPoem = 'What is a jQuery but a misunderstood object?'
 //What is a jQuery but a misunderstood object? --> What Is A JQuery But A Misunderstood Object?
 
-  //code here
+  var capitalize = function(poem){
+  	var myPoemArray = poem.split(" ");
+  	for (var i = 0; i < myPoemArray.length; i++){
+  		var firstLetter = myPoemArray[i].split("");
+  		firstLetter[0] = firstLetter[0].toUpperCase();
+  		myPoemArray[i] = firstLetter.join("");
+  	}
+  	console.log(myPoemArray.join(" "));
+  };
+  capitalize(myPoem);
 
 
 
@@ -72,3 +114,18 @@ var myPoem = 'What is a jQuery but a misunderstood object?'
 
 var theOdyssey = "function expression or function declaration? Tis an obvious choice";
 //Write a function called vowelCounter that takes in a string (theOdyssey) and returns how many vowels are in that string.
+
+var vowels = [a, e, i, o, u]
+
+var vowelCounter = function(sentence) {
+	var sentenceArray = sentence.split("");
+	for(var i = 0; i < sentenceArray.length; i++){
+
+	}
+
+};
+
+
+
+
+
--- objectReview.js
+++ objectReview.js
@@ -1,19 +1,28 @@
-//Make a 'favoriteThings' object that contains the following keys: band, food, person, book, movie, holiday. Have the values to those keys be your favorite thing in that category. 
+//Make a 'favoriteThings' object that contains the following keys: band, food, person, book, 
+//movie, holiday. Have the values to those keys be your favorite thing in that category. 
 
-  //Your code here
+  var favoriteThings = {
+    band: 'Rise Against',
+    food: 'Pizza',
+    person: 'wife',
+    book: 'As a man thinketh',
+    movie: 'American Sniper',
+    holiday: 'None',
+  }
 
 //After you've made your object, add another key named 'car' with the value being your favorite car and then another key named 'brand' with the value being your favorite brand.
 
-  //Your code here
+  favoriteThings.car = 'Tundra',
+  favoriteThings.brand = 'Volcom',
 
 //Now change the food key in your favoriteThings object to be 'Lettuce' and change the book key in your favoriteThings object to be '50 Shades of Gray'. 
 
-  //Your code here
+  favoriteThings.food = 'Pizza',
 
 //Now, alert your favorite person, then alert your favorite book.
 
-  //Your code here
-
+  alert(favoriteThings.person);
+  alert(favoriteThings.book);
 
 
 /* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
@@ -31,15 +40,23 @@ var user = {
 
 //Above you're given a user object. Loop through the user object checking to make sure that each value is truthy. If it's not truthy, remove it from the object. hint: 'delete'.
 
-  //Your code here
+  for (key in user) {
+    if(user[key]) {
+      console.log("truthy");
+    } else {
+      delete user[key];
+    }
+  }
 
 //Once you get your truthy object, Change the remaining values in the object to be specific to you (name: 'your name', username: 'your username'), rather than my information.
 
-  //Your code here
+  user.name = 'Rocky Gibson',
+  user.pwHash = 'bust',
+  user.username = 'rockygibson',
 
 //Now console.log your object and make sure it looks right.
 
-  //Your code here
+  console.log(user);
 
 
 
